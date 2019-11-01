4808ae57d72597db1c6220bd697f50a5999197a2
Malcolm Womble <malcolmaw@icloud.com>
2019-11-01 11:44:07 -0700

    add progress towards replacing numbers in range containing three with special phrase

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function()  {
     var userInput = parseInt($("input#userInput").val());
     var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     var userRange = [];
+    var three = 3
 
     for (var index = 0; index <= userInput; index++) {
       userRange.push(index);
@@ -15,6 +16,8 @@ $(document).ready(function()  {
     var beepBoop = function()  {
       if (!(numbers.includes(userInput))) {
         alert("Whoops! Please use numeric characters to enter an integer");
+      } else if (userRange.includes(three))  {
+        return userRange[index].replaceWith("I'm sorry Dave, I'm afraid I can't do that")
       } else  {
         return userRange;
       }
